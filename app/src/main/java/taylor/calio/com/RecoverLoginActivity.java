4855504bcc9696de56851029ba66c6c442e35ab1
thscs213 <h0304461@students.katyisd.org>
2016-03-22 13:38:36 -0500

    Commit  14 my computer is broken ;_;

--- app/src/main/java/taylor/calio/com/RecoverLoginActivity.java
+++ app/src/main/java/taylor/calio/com/RecoverLoginActivity.java
@@ -2,7 +2,7 @@ package taylor.calio.com;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -18,6 +18,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import taylor.calio.com.R;
 
@@ -38,6 +40,9 @@ public class RecoverLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // foxes <3
+                FireMissilesDialogFragment f = new FireMissilesDialogFragment();
+                f.onCreateDialog(savedInstanceState);
+
             }
         });
 
@@ -73,6 +78,49 @@ public class RecoverLoginActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    public static class FireMissilesDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final ArrayList mSelectedItems = new ArrayList();  // Where we track the selected items
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Set the dialog title
+            builder.setTitle("title")
+                    // Specify the list array, the items to be selected by default (null for none),
+                    // and the listener through which to receive callbacks when items are selected
+                    .setMultiChoiceItems(R.array.loginQuestions, null,
+                            new DialogInterface.OnMultiChoiceClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which,
+                                                    boolean isChecked) {
+                                    if (isChecked) {
+                                        // If the user checked the item, add it to the selected items
+                                        mSelectedItems.add(which);
+                                    } else if (mSelectedItems.contains(which)) {
+                                        // Else, if the item is already in the array, remove it
+                                        mSelectedItems.remove(Integer.valueOf(which));
+                                    }
+                                }
+                            })
+                            // Set the action buttons
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK, so save the mSelectedItems results somewhere
+                            // or return them to the component that opened the dialog
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+
+            return builder.create();
+        }
     }
 
 }
